d879c082928ec4ae11ecd67817e43b3d947a0e1c
MatheusSaggiorato <matheussaggiorato@gmail.com>
2023-01-24 02:15:43 -0300

    teste

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -75,29 +75,76 @@ function BackToTopFunction() {
 
 //Carousel
 
-window.addEventListener('load', function(){
-  new Glider(document.querySelector('.glider'), {
-    // Mobile-first defaults
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    scrollLock: true,
-    dots: '#dots',
-    arrows: {
-      prev: '.glider-prev',
-      next: '.glider-next'
-    },
+
+$(document).ready(function(){
+  $('.responsive').slick({
+    dots: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    draggable: true,
     responsive: [
       {
-        // screens greater than >= 775px
-        breakpoint: 650,
+        breakpoint: 1024,
         settings: {
           slidesToShow: 2,
-          slidesToScroll: 1,
-          itemWidth: 150,
-          duration: 0.25,
-          dots: '.dots'
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true
         }
       },
+      {
+        breakpoint: 700,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ]
+  });
+});
+
+
+
+
+
+
+
+
+
+
+$(document).ready(function(){
+  $('.testimonyal-container').slick({
+    dots: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    draggable: true,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
     ]
   });
 });
