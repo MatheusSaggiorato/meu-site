9874ad702bb74b3482e92feff187ce905bca46bb
MatheusSaggiorato <matheussaggiorato@gmail.com>
2023-02-04 00:52:05 -0300

    teste

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -73,41 +73,8 @@ function BackToTopFunction() {
 }
 
 
+
 //Carousel
-
-
-$(document).ready(function(){
-  $('.responsive').slick({
-    dots: true,
-    infinite: true,
-    speed: 300,
-    slidesToShow: 4,
-    slidesToScroll: 4,
-    draggable: true,
-    responsive: [
-      {
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 3,
-          infinite: true,
-          dots: true
-        }
-      },
-      {
-        breakpoint: 700,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1
-        }
-      }
-      // You can unslick at a given breakpoint now by adding:
-      // settings: "unslick"
-      // instead of a settings object
-    ]
-  });
-});
-
 $(document).ready(function(){
   $('.testimonyal-container').slick({
     dots: true,
