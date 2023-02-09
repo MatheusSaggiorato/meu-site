af909b370513fbf32879693c9a927deb90d8cea6
MatheusSaggiorato <matheussaggiorato@gmail.com>
2023-02-09 00:27:14 -0300

    teste

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -75,6 +75,8 @@ $(document).ready(function () {
     speed: 300,
     slidesToShow: 4,
     slidesToScroll: 4,
+    autoplay: true,
+    autoplaySpeed: 2000,
     draggable: true,
     responsive: [
       {
